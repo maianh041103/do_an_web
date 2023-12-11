2cd3ccfb424049da64f4563fabd0a6f9d4c9a298
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-11 16:49:52 +0700

    Update checkout

--- api/v1/controller/client/checkout.controller.js
+++ api/v1/controller/client/checkout.controller.js
@@ -7,82 +7,19 @@ const getStockProductByIdHelper = require('../../../../helper/getStockProductByI
 const calcPriceNewHelper = require('../../../../helper/calcPriceNew.helper');
 const getProductHelper = require('../../../../helper/getProduct.helper');
 
-//[POST] /api/v1/checkout
+const getListProductHelper = require('../../../../helper/getListProduct.helper');
+
+//[GET] /api/v1/checkout
 module.exports.checkout = async (req, res) => {
   //Lấy ra sản phẩm 
-  let totalPrice = 0; //Lưu tổng số tiền trước khi áp mã giảm giá
-  const listProductId = req.body.products;
+  const listProduct = req.body.products;
 
-  let products = [];
-  for (let i = 0; i < listProductId.length; i++) {
-    let product = {
-      product_id: listProductId[i].product_id,
-      childTitle: listProductId[i].childTitle,
-      quantity: listProductId[i].quantity
-    }
-    let inforProduct = await Product.findOne({
-      _id: listProductId[i].product_id,
-      deleted: false
-    });
-    inforProduct = await getProductHelper.getProduct(inforProduct);
-    let newInforProduct = {
-      title: inforProduct.title,
-      description: inforProduct.description,
-      images: inforProduct.images,
-      price: inforProduct.price,
-      stock: inforProduct.stock,
-      quantity: inforProduct.quantity,
-      featured: inforProduct.featured,
-      status: inforProduct.status,
-      properties: inforProduct.properties,
-      deleted: inforProduct.deleted,
-      slug: inforProduct.slug,
-      rate: inforProduct.rate,
-      discountPercent: inforProduct.discountPercent,
-      //minPrice: inforProduct.minPrice,
-      //buyed: inforProduct.buyed,
-      productCategoryTitle: inforProduct.productCategoryTitle
-    }
-    if (listProductId[i].childTitle !== "none") {
-      let productChild = inforProduct.newGroup.find(item => {
-        return item.childTitle === listProductId[i].childTitle;
-      })
-      newInforProduct.productChild = productChild;
-      console.log(productChild.priceNew);
+  let products = await getListProductHelper.getListProductsCart(listProduct);
 
-      totalPrice = productChild.priceNew * newInforProduct.quantity;
-      product.totalPrice = totalPrice;
-    }
-    product.inforProduct = newInforProduct;
+  const totalPriceOrder = products.reduce((calc, item) => {
+    return calc + item.totalPrice;
+  }, 0);
 
-
-
-    products.push(product);
-
-
-
-    // let productData = await Product.findOne({
-    //   _id: listProductId[i].product_id,
-    //   deleted: false
-    // });
-
-    // productData = calcPriceNewHelper.calc(productData);
-    // if (listProductId[i].childTitle === "none") {
-    //   listProductId[i].infoProduct = productData;
-    //   totalPrice += listProductId[i].infoProduct.priceNew * listProductId[i].quantity;
-    // } else {
-    //   const productChild = productData.group.find(item => {
-    //     return item.childTitle === listProductId[i].childTitle;
-    //   })
-    //   const data = await Product.findOne({
-    //     _id: listProductId[i].product_id,
-    //     deleted: false
-    //   }).select("-group");
-    //   data.productChild = productChild;
-    //   listProductId[i].infoProduct = data;
-    //   totalPrice += listProductId[i].infoProduct.productChild.priceNew * listProductId[i].quantity;
-    //}
-  }
   //End lấy ra sản phẩm
 
   //Lấy ra mã giảm giá 
@@ -103,8 +40,7 @@ module.exports.checkout = async (req, res) => {
   //End lấy ra mã giảm giá
 
   res.json({
-    // totalPrice: totalPrice,
-    // listProductId: listProductId,
+    totalPriceOrder: totalPriceOrder,
     listDiscount: listDiscount,
     user: req.user,
     products: products
@@ -119,27 +55,9 @@ module.exports.order = async (req, res) => {
       account_id: req.user.id
     });
 
-    const products = req.body.products;
-    for (let i = 0; i < products.length; i++) {
-      const productData = await Product.findOne({
-        _id: products[i].product_id,
-        deleted: false
-      });
-      if (!productData.discountPercent)
-        productData.discountPercent = 0;
-      if (products[i].childTitle === "none") {
-        products[i].price = productData.price;
-        products[i].discountPercent = productData.discountPercent;
-        products[i].priceNew = parseInt((products[i].price * (100 - products[i].discountPercent) / 100).toFixed(0));
-      } else {
-        const productChild = productData.group.find(item => {
-          return item.childTitle === products[i].childTitle;
-        })
-        products[i].price = productChild.price;
-        products[i].discountPercent = productData.discountPercent;
-        products[i].priceNew = parseInt((products[i].price * (100 - products[i].discountPercent) / 100).toFixed(0));
-      }
-    }
+    const listProduct = req.body.products;
+
+    let products = await getListProductHelper.getListProductsCart(listProduct);
 
     const data = {
       cart_id: cart.id,
@@ -149,7 +67,7 @@ module.exports.order = async (req, res) => {
         address: req.body.address
       },
       products: products,
-      discountId: req.body.discountId
+      discountId: req.body.discountId,
     }
 
     const newOrder = new Order(data);
@@ -186,7 +104,8 @@ module.exports.order = async (req, res) => {
 
     res.json({
       code: 200,
-      message: "Đặt hàng thành công"
+      message: "Đặt hàng thành công",
+      data: data
     });
   } catch (error) {
     console.log(error);
@@ -195,5 +114,4 @@ module.exports.order = async (req, res) => {
       message: "Đặt hàng thất bại"
     });
   }
-
 }
--- helper/getListProduct.helper.js
+++ helper/getListProduct.helper.js
@@ -1,11 +1,13 @@
 const ProductCategory = require('../api/v1/models/productCategory.model');
+const Product = require('../api/v1/models/product.model');
 
 const calcPriceNew = require('./calcPriceNew.helper');
 const minPriceHelper = require('./findMinPrice.helper');
 const productsBestSellerHelper = require('./productBestSeller.helper');
+const getProductHelper = require('./getProduct.helper');
 
 
-module.exports.getListProduct = async (products)=>{
+module.exports.getListProduct = async (products) => {
   let resultProduct = [];
   for (let product of products) {
     let newProduct = {
@@ -39,7 +41,60 @@ module.exports.getListProduct = async (products)=>{
       newProduct.productCategoryTitle = productCategory.title;
     else
       newProduct.productCategoryTitle = "";
-      resultProduct.push(newProduct);
+    resultProduct.push(newProduct);
   }
   return resultProduct;
+}
+
+
+module.exports.getListProductsCart = async (listProductId) => {
+  let products = [];
+  for (let i = 0; i < listProductId.length; i++) {
+    let product = {
+      product_id: listProductId[i].product_id,
+      childTitle: listProductId[i].childTitle,
+      quantity: listProductId[i].quantity
+    }
+    let inforProduct = await Product.findOne({
+      _id: listProductId[i].product_id,
+      deleted: false
+    });
+    inforProduct = await getProductHelper.getProduct(inforProduct);
+    let newInforProduct = {
+      title: inforProduct.title,
+      description: inforProduct.description,
+      images: inforProduct.images,
+      price: inforProduct.price,
+      stock: inforProduct.stock,
+      quantity: inforProduct.quantity,
+      featured: inforProduct.featured,
+      status: inforProduct.status,
+      properties: inforProduct.properties,
+      deleted: inforProduct.deleted,
+      slug: inforProduct.slug,
+      rate: inforProduct.rate,
+      discountPercent: inforProduct.discountPercent,
+      priceNew: inforProduct.priceNew,
+      //minPrice: inforProduct.minPrice,
+      //buyed: inforProduct.buyed,
+      productCategoryTitle: inforProduct.productCategoryTitle
+    }
+    if (listProductId[i].childTitle !== "none") {
+      let productChild = inforProduct.newGroup.find(item => {
+        return item.childTitle === listProductId[i].childTitle;
+      })
+      newInforProduct.productChild = productChild;
+
+      let totalPrice = productChild.priceNew * product.quantity;
+      product.totalPrice = totalPrice;
+    }
+    else {
+      let totalPrice = newInforProduct.priceNew * product.quantity;
+      product.totalPrice = totalPrice;
+    }
+    product.inforProduct = newInforProduct;
+
+    products.push(product);
+  }
+  return products;
 }
