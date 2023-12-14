9b5cf73eed90c19592042798639699ac91f26e7e
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-14 18:21:07 +0700

    Test

--- data/settingGeneral.data.js
+++
@@ -1,8 +0,0 @@
-[{
-  "websiteName": "Web Team Mõm",
-  "logo": "",
-  "phone": "0988718278",
-  "email": "nguyenmaianh041103@gmail.com",
-  "address": "Số 55 đường Giải Phóng quận Hai Bà Trưng thành phố Hà Nội",
-  "copyright": "Copyright"
-}]
