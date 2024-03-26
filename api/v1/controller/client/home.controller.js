d80bc9b55375a5342f22b517897bf42aea7ed0ea
maianh041103 <nguyenmaianh041103@gmail.com>
2024-03-26 20:02:31 +0700

    Update home

--- api/v1/controller/client/home.controller.js
+++ api/v1/controller/client/home.controller.js
@@ -6,6 +6,99 @@ const productsBestSellerHelper = require('../../../../helper/productBestSeller.h
 const calcPriceNewHelper = require('../../../../helper/calcPriceNew.helper');
 const getListProductHelper = require('../../../../helper/getListProduct.helper');
 
+//[GET] /api/v1/home/productCategory
+module.exports.productCategory = async (req, res) => {
+  try {
+    //Lấy ra danh mục sản phẩm
+    const productCategorys = await ProductCategory.find({
+      deleted: false,
+      status: "active"
+    })
+    //End lấy ra danh mục sản phẩm
+    res.json({
+      code: 200,
+      productCategorys: productCategorys
+    })
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi"
+    })
+  }
+}
+
+//[GET] /api/v1/home/productBestSellers
+module.exports.productBestSellers = async (req, res) => {
+  try {
+    //Lấy ra sản phẩm bán chạy nhất
+    const productBestSellers = await Product.find({
+      deleted: false,
+      status: "active"
+    })
+
+    let newProductBestSellers = await getListProductHelper.getListProduct(productBestSellers);
+
+    newProductBestSellers.sort((a, b) => {
+      return b.productSold - a.productSold;
+    })
+    res.json({
+      code: 200,
+      newProductBestSellers: newProductBestSellers
+    })
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi"
+    })
+  }
+}
+
+//[GET] /api/v1/home/productFeatureds
+module.exports.productFeatureds = async (req, res) => {
+  try {
+    //Lấy ra sản phẩm nổi bật
+    const productFeatureds = await Product.find({
+      deleted: false,
+      status: "active",
+      featured: "1"
+    })
+
+    let newProductFeatureds = await getListProductHelper.getListProduct(productFeatureds);
+    res.json({
+      code: 200,
+      newProductFeatureds: newProductFeatureds
+    })
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi"
+    })
+  }
+}
+
+//[GET] /api/v1/home/productBestRate
+module.exports.productBestRate = async (req, res) => {
+  try {
+    //Lấy ra sản phẩm sắp xếp theo rate
+    const productBestRate = await Product.find({
+      deleted: false,
+      status: "active"
+    }).sort({ rate: "desc" }).limit(3);
+
+    let newProductBestRate = await getListProductHelper.getListProduct(productBestRate);
+    ///End lấy sản phẩm sản phẩm theo rate
+    res.json({
+      code: 200,
+      newProductBestRate: newProductBestRate
+    })
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi"
+    })
+  }
+}
+
 //[GET] /api/v1/
 module.exports.index = async (req, res) => {
   //Lấy ra danh mục sản phẩm
@@ -43,7 +136,7 @@ module.exports.index = async (req, res) => {
     deleted: false,
     status: "active"
   }).sort({ rate: "desc" }).limit(3);
-  
+
   let newProductBestRate = await getListProductHelper.getListProduct(productBestRate);
   ///End lấy sản phẩm sản phẩm theo rate
 
--- api/v1/router/client/home.route.js
+++ api/v1/router/client/home.route.js
@@ -4,4 +4,12 @@ const controller = require('../../controller/client/home.controller');
 
 route.get('/', controller.index);
 
+route.get('/home/productCategory', controller.productCategory);
+
+route.get('/home/productBestSellers', controller.productBestSellers);
+
+route.get('/home/productFeatureds', controller.productFeatureds);
+
+route.get('/home/productBestRate', controller.productBestRate);
+
 module.exports = route;
